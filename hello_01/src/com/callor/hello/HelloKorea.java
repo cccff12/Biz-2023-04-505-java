ef48505a9e0c0dda5d62602ed3f230daeddc5504
cccff12 <ksun0430@naver.com>
2023-04-13 10:17:44 +0900

    새로운_리파지토리

---
+++ hello_01/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,14 @@
+package com.callor.hello;
+
+
+public class HelloKorea {
+	public static void main(String[] args) {
+		System.out.println(10.0+11.0);
+		System.out.println(10.0-11.0);
+		System.out.println(10.0*11.0);
+		System.out.println(10.0/11.0);
+	
+	
+	
+	}
+}
