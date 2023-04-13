887abf9733ecb7643b784033a6f69bf5a0a34fad
cccff12 <ksun0430@naver.com>
2023-04-13 16:40:33 +0900

    jaja

---
+++ hello_01/src/com/callor/hello/alzobra/BooleanA.java
@@ -0,0 +1,16 @@
+package com.callor.hello.alzobra;
+
+public class BooleanA {
+	
+	public static void main(String[] args) {
+	
+		System.out.println(30 > 40);
+		System.out.println(30 < 40);
+		System.out.println(30 > 30);
+		System.out.println(30 >= 30);
+		System.out.println(30 <= 30);
+		System.out.println(30 == 30);
+		
+		
+	}
+}
