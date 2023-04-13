ad15f49132d570f361b4a827d61fa67036dd4ff9
cccff12 <ksun0430@naver.com>
2023-04-13 14:17:48 +0900

    이모지

--- hello_01/src/com/callor/hello/HelloYour.java
+++ hello_01/src/com/callor/hello/HelloYour.java
@@ -4,16 +4,13 @@ package com.callor.hello;
  *  Source Code, Java Class File
  */
 
-
 public class HelloYour {
-	
 	public static void main(String[] args) {
-		System.out.println(30+40);
-		System.out.println(30-40);
-		System.out.println(30*40);
-		System.out.println(30/40);
-		System.out.println(30%40);
+		System.out.println(30 + 40);
+		System.out.println(30 - 40);
+		System.out.println(30 * 40);
+		System.out.println(30 / 40);
+		System.out.println(30 % 40);
 	}
-	
 
 }
