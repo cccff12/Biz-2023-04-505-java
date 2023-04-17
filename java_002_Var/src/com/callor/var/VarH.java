a4e298c8ccbce04efd5a6149644097ed15defd2c
cccff12 <ksun0430@naver.com>
2023-04-17 16:42:27 +0900

    for문

---
+++ java_002_Var/src/com/callor/var/VarH.java
@@ -0,0 +1,23 @@
+package com.callor.var;
+
+public class VarH {
+	public static void main(String[] args) {
+		int num = 0;
+		num = num + 1;
+		num += 1;
+		num++; // num 변수에 저장된 값을 1 증가
+
+		num = num - 1;
+		num -= 1;
+		num--; // num 변수에 저장된 값을 1감수
+
+		
+		
+		num++;
+		num++;
+		num++;
+		num++;
+		num++;
+		
+	}
+}
