b2b62a00fe80bd23ea9cc1abb23c9adbc117f8a9
cccff12 <ksun0430@naver.com>
2023-04-17 09:48:41 +0900

    var

---
+++ java_002_Var/src/com/callor/var/VarB.java
@@ -0,0 +1,17 @@
+package com.callor.var;
+
+public class VarB {
+	public static void main(String[] args) {
+		int num1 = 0;
+		int num2 = 0;
+
+		num1 = 30;
+		num2 = 40;
+
+		int sum1 = num1 + num2;
+
+		System.out.println(sum1);
+
+	}
+
+}
