2501d930faaa516e5eb2e1d15d46188b3929aaf8
cccff12 <ksun0430@naver.com>
2023-04-25 16:40:50 +0900

    배열2

--- java_008_class/src/com/callor/classes/arrays/ArrayF.java
+++ java_008_class/src/com/callor/classes/arrays/ArrayF.java
@@ -1,7 +1,10 @@
 package com.callor.classes.arrays;
 
 public class ArrayF {
-
+/*
+ * prime()method에게 정수값 1개를 전달하고 
+ * 그 정수값이 소수인지 아닌지 알려달라 요청한것
+ */
 	public static int prime(int ran) {
 
 		int i;
@@ -14,7 +17,14 @@ public class ArrayF {
 		return ran;
 
 	}
-
+	/*
+	 * 다수의 데이터를 정보처리할 경우 데이터를 수집하고, 수지빙 완료되었을때
+	 * 한꺼번에 처리를 한다
+	 * 
+	 * 실시간 처리: 데이터가 발생하는 즉시 처리
+	 * 배치(Batch)처리: 데이터를 모두 수집하고 수집이 완료되었을때 처리 ex) 급여계산, 인구통계
+	 * 
+	 */
 	public static void main(String[] args) {
 
 		int[] num = new int[10];
@@ -23,10 +33,11 @@ public class ArrayF {
 			num[i] = (int) (Math.random() * 50) + 51;
 		}
 		for (int i = 0; i < 10; i++) {
-			if(prime(num[i])>0) {
-				System.out.println(num[i]+"소수");
-			}else {System.out.println(num[i]+"소수아님");
-				
+			if (prime(num[i]) > 0) {
+				System.out.println(num[i] + "소수");
+			} else {
+				System.out.println(num[i] + "소수아님");
+
 			}
 		}
 	}
---
+++ java_008_class/src/com/callor/classes/arrays/test.java
@@ -0,0 +1,35 @@
+package com.callor.classes.arrays;
+
+public class test {
+
+	public static int prime(int ran) {
+
+		for (int i = 2; i < ran; i++) {
+			if (ran % i == 0) {
+				return 0;
+			}
+
+		}
+		return ran;
+
+	}
+
+	public static void main(String[] args) {
+
+		int[] num = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			num[i] = (int) (Math.random() * 50) + 51;
+
+		}
+
+		for (int i = 0; i < 10; i++) {
+			if (prime(num[i]) > 0) {
+				System.out.println(num[i] + "는 소수이다");
+			}else {
+				System.out.println(num[i] + "는 소수가 아니다");
+			}
+		}
+
+	}
+}
