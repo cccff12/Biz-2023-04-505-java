abb4b0e3f2632dee67b0eff59024c4beed75f297
cccff12 <ksun0430@naver.com>
2023-04-27 15:40:55 +0900

    sort4

--- Java_OneDay_001_Score/src/com/callor/score/oneday.java
+++ Java_OneDay_001_Score/src/com/callor/score/oneday.java
@@ -6,18 +6,19 @@ import scoreDto.ScoreDto;
 public class oneday {
 
 	public static void main(String[] args) {
-
+// servicea 메서드를 호출해 ser라는 이름으로 설정
 		servicea ser = new servicea();
-
+//ScoreDto 타입을 score 라는 이름으로 10개의 배열을 설정
 		ScoreDto[] score = new ScoreDto[10];
-
+//nums라는 이름으로 10개의 배열을 설정 
 		String[] nums = { "23001", "23002", "23003", "23004", "23005", 
 				"23006", "23007", "23008", "23009", "23010" };
-
+// for()문으로 score배열을 전부 ScoreDto로 초기화
 		for (int i = 0; i < score.length; i++) {
 			score[i] = new ScoreDto();
 		}
-
+//for()문을 사용해 score배열에 전부 service 매서드로 각 과목마다
+		//랜덤값 부여
 		for (int i = 0; i < score.length; i++) {
 			score[i].kor = ser.ran();
 			score[i].eng = ser.ran();
@@ -26,11 +27,13 @@ public class oneday {
 			score[i].art = ser.ran();
 			score[i].num = nums[i];
 		}
+		//각 과목의 총점을 저장할 변수를 초기화
 		int korall = 0;
 		int engall = 0;
 		int mathall = 0;
 		int musicall = 0;
 		int artall = 0;
+		
 		for (int i = 0; i < score.length; i++) {
 			korall += score[i].kor;
 			engall += score[i].eng;
@@ -38,6 +41,7 @@ public class oneday {
 			musicall += score[i].music;
 			artall += score[i].art;
 		}
+		
 		int all = korall + engall + mathall + musicall + artall;
 		float koravg = (float) korall / 10;
 		float engavg = (float) engall / 10;
@@ -55,12 +59,10 @@ public class oneday {
 		for (int i = 0; i < score.length; i++) {
 			ser.print(score[i]);
 		}
+		ser.addprint(korall, engall, mathall, musicall, artall, all);
+		ser.avgprint(koravg, engavg, mathavg, musicavg, artavg, allavg);
 		System.out.println("-".repeat(80));
-		System.out.printf("총점\t%d\t%d\t%d\t%d\t%d\t%d\n", 
-				korall, engall, mathall, musicall, artall, all);
-		System.out.printf("평균\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t\t%.2f\n",
-				koravg, engavg, mathavg, musicavg, artavg,
-				allavg);
+		
 		System.out.println("=".repeat(80));
 
 	}
--- Java_OneDay_001_Score/src/method/servicea.java
+++ Java_OneDay_001_Score/src/method/servicea.java
@@ -4,7 +4,6 @@ import scoreDto.ScoreDto;
 
 public class servicea {
 
-	
 	public int ran() {
 		int ran = (int) (Math.random() * 50) + 51;
 		return ran;
@@ -20,17 +19,13 @@ public class servicea {
 		System.out.printf("%3d\t", score.total());
 		System.out.printf("%5.2f\n", score.avg());
 	}
-	
-	
-	
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	public void addprint(int num1, int num2, int num3, int num4, int num5, int num6) {
+		System.out.printf("총점\t%d\t%d\t%d\t%d\t%d\t%d\n", num1, num2, num3, num4, num5, num6);
+	}
+
+	public void avgprint(float num1, float num2, float num3, float num4, float num5, float num6) {
+		System.out.printf("평균\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t\t%.2f\n", num1, num2, num3, num4, num5, num6);
+	}
+
+}
