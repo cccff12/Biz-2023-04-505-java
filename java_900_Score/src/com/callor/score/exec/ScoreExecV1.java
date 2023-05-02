c33b3ca74a72d0bbd3f7317c7146a3f37d50fa09
cccff12 <ksun0430@naver.com>
2023-05-02 10:59:01 +0900

    5.2.2

--- java_900_Score/src/com/callor/score/exec/ScoreExecV1.java
+++ java_900_Score/src/com/callor/score/exec/ScoreExecV1.java
@@ -1,5 +1,29 @@
 package com.callor.score.exec;
 
+import com.callor.score.model.ScoreDto;
+import com.callor.score.service.ScoreServiceV1;
+
 public class ScoreExecV1 {
+
+	public static void main(String[] args) {
+		// 10명 학생의 점수를 저장할 객체배열 선언
+		ScoreDto[] scoreList = new ScoreDto[10];
+		// 객체배열 초기화 하기
+		for (int i = 0; i < scoreList.length; i++) {
+			scoreList[i] = new ScoreDto();
+		}
+		// ScoreService에 있는 method를 사용하기 위한 준비
+		ScoreServiceV1 scService = new ScoreServiceV1();
+		// ScoreService 클래스의 scoreList객체 배열변수에 
+		// 생성된 (선언과 초기화가 완료된) scoreList 객체 배열값을
+		// 세팅하는 절차
+		scService.setScoreList(scoreList);
+		// 점수 생성하기
+		scService.scoreRndMake();
+		//점수 리스트 생성하기
+		scService.scoreListPrint();
+		scService.scoreTotalPrint();
+		scService.scoreAvgPrint();
+	}
 
 }
