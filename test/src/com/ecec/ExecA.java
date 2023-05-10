0ad478d3ccdbb08c1803c3a871ace97b7a7a01c3
cccff12 <ksun0430@naver.com>
2023-05-10 17:50:50 +0900

    5.10.3

--- test/src/com/ecec/ExecA.java
+++ test/src/com/ecec/ExecA.java
@@ -1,5 +1,11 @@
 package com.ecec;
 
-public class ExecA {
+import com.service.serviceA;
 
+public class ExecA {
+public static void main(String[] args) {
+	serviceA serA= new ExecA();
+	
+	
+}
 }
--- test/src/com/service/serviceA.java
+++ test/src/com/service/serviceA.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.DTO.index;
 import com.DTO.scoreDto;
 
 public class serviceA {
@@ -25,18 +26,26 @@ public class serviceA {
 		InputStream is = null;
 		
 		try {
-			is = new FileInputStream(str);
+			is = new FileInputStream(str);//
 		} catch (FileNotFoundException e) {
 			System.out.println("뭔가 잘못됐다");
 			e.printStackTrace();
 		}
 		scan= new Scanner(is);
 		while(scan.hasNext()) {
-			
-			
-			
-			
-			
+			String line = scan.nextLine();//
+			String[] score= line.split(",");
+			scoreDto scDto= new scoreDto(
+			scDto.num1=score[index.scores.st_num],
+					scDto.num2=score[index.scores.st_num2],
+			score[index.scores.st_num3],
+			score[index.scores.st_num4],
+			score[index.scores.st_num5],
+			score[index.scores.st_num6],
+			score[index.scores.st_num7]
+					);
+		list.add(scDto);
+		
 		}
 		
 		
